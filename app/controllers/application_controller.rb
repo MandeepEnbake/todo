6ccace4c975b93bce65c9d832c16419e27748011
Mandeep Rawat <mandeep.enbake@gmail.com>
2015-12-31 12:25:31 +0530

    user authntication working properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :authenticate_user!
   protect_from_forgery with: :exception
+  layout :layout_by_controller
+
+    protected
+
+    def layout_by_controller
+      devise_controller? ? 'devise' : 'application'
+    end
 end
